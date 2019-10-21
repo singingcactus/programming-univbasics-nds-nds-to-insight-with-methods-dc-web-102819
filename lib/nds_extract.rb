af82e66a478b1191b5bb9d43f2fc98a60ad76bbe
Matt Bechtel <bechma02@gmail.com>
2019-10-21 17:32:22 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -7,16 +7,24 @@ require 'directors_database'
 
 def directors_totals(nds)
   director_index = 0
-  result = {}
+
 
   while director_index < nds.length do
-      director_name = nds[director_index][:name]
+      director_name[director_index] = nds[director_index][:name]
       director_hash = nds[director_index]
-      result[director_name] << gross_for_director(director_hash)
       director_index += 1
   end
 
-  result
+  result = {
+    director_name[0] => gross_for_director(nds[0]),
+    director_name[1] => gross_for_director(nds[1]),
+    director_name[2] => gross_for_director(nds[2]),
+    director_name[3] => gross_for_director(nds[3]),
+    director_name[4] => gross_for_director(nds[4]),
+    director_name[5] => gross_for_director(nds[5]),
+    director_name[6] => gross_for_director(nds[6]),
+    director_name[7] => gross_for_director(nds[7]),
+  }
 end
 
 # Find a way to accumulate the :worldwide_grosses and return that Integer
